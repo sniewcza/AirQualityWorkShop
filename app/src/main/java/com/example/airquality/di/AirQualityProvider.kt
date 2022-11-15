52e9a649e43069c717486256a49503f71b1cc3b0
Szymon Niewczas <szymon_niewczas@wp.pl>
2022-11-15 08:34:53 +0100

    rest api integration

--- app/src/main/java/com/example/airquality/di/AirQualityProvider.kt
+++ app/src/main/java/com/example/airquality/di/AirQualityProvider.kt
@@ -1,12 +1,16 @@
 package com.example.airquality.di
 
-import com.example.airquality.logic.FakeRemoteStationsRepository
-import com.example.airquality.logic.GetStationsUseCase
+import com.example.airquality.data.AirlyStationDataSource
+import retrofit2.converter.gson.GsonConverterFactory
 import com.example.airquality.logic.RemoteStationsRepository
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.Response
+import retrofit2.Retrofit
 import javax.inject.Singleton
 
 @Module
@@ -15,7 +19,46 @@ object AirQualityProvider {
 
     @Provides
     @Singleton
-    fun provideRemoteStationsRepository(): RemoteStationsRepository {
-        return FakeRemoteStationsRepository()
+    fun provideRemoteStationsRepository(airlyService: AirlyStationDataSource.AirlyService): RemoteStationsRepository {
+        return AirlyStationDataSource(airlyService)
+    }
+
+    @Provides
+    @Singleton
+    fun provideAirlyAuthOkHttpClient(): OkHttpClient {
+        return OkHttpClient
+            .Builder()
+            .addInterceptor(AirlyAuthInterceptor())
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(
+        okHttpClient: OkHttpClient
+    ): Retrofit {
+        return Retrofit
+            .Builder()
+            .client(okHttpClient)
+            .addConverterFactory(GsonConverterFactory.create())
+            .baseUrl(AirlyStationDataSource.HOST)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideAirlyService(retrofit: Retrofit): AirlyStationDataSource.AirlyService {
+        return retrofit
+            .create(AirlyStationDataSource.AirlyService::class.java)
+    }
+
+
+    class AirlyAuthInterceptor : Interceptor {
+        override fun intercept(chain: Interceptor.Chain): Response {
+            val requestBuilder = chain.request().newBuilder()
+            requestBuilder.addHeader("apikey", "HW3cvqosIpfPsFPLlOFmiLeZ6X4rB57L")
+            return chain.proceed(requestBuilder.build())
+        }
+
     }
 }
